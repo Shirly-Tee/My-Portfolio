f2b18c4f84d2638042966aabc0c26dbcf7104777
Shirly-Tee <taiwomary108@gmail.com>
2023-11-18 02:43:33 +0100

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const navbtn =  document.querySelector('.navbtn');
+const sidebar = document.querySelector('.sidebar');
+
+
+navbtn.addEventListener('click',()=>{
+
+
+    sidebar.classList.toggle('active')
+})
